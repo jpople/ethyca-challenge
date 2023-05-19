811c2901d32738040434031f61700b9ffffa9a91
Jeremy Pople <jpople@gmail.com>
2023-05-19 13:19:41 -0400

    Update fonts to be a little prettier

--- src/SystemColumn.tsx
+++ src/SystemColumn.tsx
@@ -11,7 +11,7 @@ export default function SystemColumn ({systemType, data}: ColProps) {
     const filteredData = data.filter(item => item.system_type === systemType);
     const pluralizedHeading = systemType += "s"; // I can get away with this here, but in "real life" it'd be better to let a library handle this just in case
     return <Col className='d-flex flex-column align-items-center mb-4'>
-        <h1>{pluralizedHeading}</h1>
+        <h2>{pluralizedHeading}</h2>
         <div className='d-flex flex-column align-items-center' style={{width: '100%'}}>
             {filteredData.map((item, index) => <SystemCard system={item} key={index}/>)}
         </div>
